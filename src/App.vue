b8b370e9b5fdc05c175f7bc8bc3d2e1a473259cf
Ramunas <zamrif@gmail.com>
2020-09-17 20:59:25 +0300

    header router added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app">
     <Header />
-    <div id="nav">
-      <router-link to="/login">login</router-link> |
-      <router-link to="/register">register</router-link> |
-
-      <router-link to="/expences">expences</router-link> |
-      <router-link to="/income">income</router-link> |
-      <router-link to="/log">log</router-link> |
-      <router-link to="/summary">summary</router-link> |
-    </div>
     <router-view />
   </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,42 @@
 <template>
-  <div class="header">
-    <div class="wrapper">
-      <img
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.tech.co%2Fwp-content%2Fuploads%2F2015%2F07%2Ftech-02-acorns.png&f=1&nofb=1"
-        alt="logo"
-      />
-      <nav>
-        <span v-if="loggedIn">
-          <router-link to="/students">Students</router-link>
-          <router-link to="/add">+Student </router-link>
-          <router-link to="/groups">Groups </router-link>
-          <router-link to="/addGroup">+Group </router-link>
-          <a class="blue" v-on:click="signOut()">Sign Out</a>
-        </span>
+  <div id="Header">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a href="/">
+          <img
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.tech.co%2Fwp-content%2Fuploads%2F2015%2F07%2Ftech-02-acorns.png&f=1&nofb=1"
+            alt="logo"
+          />
+        </a>
 
-        <span v-else>
-          <router-link to="/register">Register</router-link>
-          <router-link class="blue" to="/login">Login</router-link>
-        </span>
-      </nav>
-    </div>
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <span v-if="loggedIn">
+            <router-link to="/expences">Expences</router-link>
+            <router-link to="/income">Income </router-link>
+            <router-link to="/log">Log </router-link>
+            <router-link to="/summary">Summary </router-link>
+            <a class="blue" v-on:click="signOut()">Sign Out</a>
+          </span>
+
+          <span v-else>
+            <router-link to="/register">Register</router-link>
+            <router-link class="blue" to="/login">Login</router-link>
+          </span>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
@@ -35,6 +52,23 @@ export default {
 </script>
 
 <style scoped>
+.navbar-brand {
+  padding: 0.5em 5em;
+  width: 100%;
+  background: lightgreen;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.navbar-menu {
+  flex-grow: 0;
+}
+
+.navbar-menu a {
+    color: darkgreen;
+    padding-left: 0.5em;
+}
 img {
   width: 100px;
 }
