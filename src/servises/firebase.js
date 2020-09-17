a5b5b5427e36c58cd8bee41604d329f34b34e2c7
Ramunas <zamrif@gmail.com>
2020-09-17 20:12:27 +0300

    firebase + bulma added

---
+++ src/servises/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAcZ9WxospAAwCsuD3HJ5rCfuaC43x0IPg",
+  authDomain: "muny-development.firebaseapp.com",
+  databaseURL: "https://muny-development.firebaseio.com",
+  projectId: "muny-development",
+  storageBucket: "muny-development.appspot.com",
+  messagingSenderId: "679616263105",
+  appId: "1:679616263105:web:ca4c77d45f5a21067ab540",
+};
+
+firebase.initializeApp(firebaseConfig);
