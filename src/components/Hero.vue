b3a7084138e7bec24507d638730c17ce29a1475f
Ramunas <zamrif@gmail.com>
2020-09-18 13:10:27 +0300

    hero +

---
+++ src/components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="hero">
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h2 class="title">
+            {{ pageTitle + " " + "page" }}
+          </h2>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+  computed: {
+    pageTitle() {
+      return this.$route.name;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -2,16 +2,43 @@
   <div id="login">
     <div class="section">
       <div class="container">
-        <h3>this is login</h3>
+        <Hero />
+
+        <form name="login">
+          <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="e.g Alex Smith" />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+              <input
+                class="input"
+                type="email"
+                placeholder="e.g. alexsmith@gmail.com"
+              />
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Hero from "../../components/Hero";
+
 export default {
   name: "Login",
+  components: { Hero },
 };
 </script>
 
-<style scoped></style>>
+<style scoped>
+h3 {
+  margin-bottom: 3em;
+}</style
+>>
