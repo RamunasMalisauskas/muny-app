dc247f10d26873a46dd43fe4176d69b522717c90
Ramunas <zamrif@gmail.com>
2020-09-23 16:05:55 +0300

    log functionality +

--- src/views/app/Log.vue
+++ src/views/app/Log.vue
@@ -7,14 +7,18 @@
         <div class="columns is-centered">
           <div class="column is-half">
             <div class="control">
-              <button class="button" :class="loading && `is-loading`">
+              <button
+                class="button"
+                @click="get()"
+                :class="loading && `is-loading`"
+              >
                 Get info
               </button>
             </div>
 
             <div class="card">
               <div class="top">
-                <p class="title is-6-desktop">2000</p>
+                <p class="title is-6-desktop">{{ this.plusMinus }}</p>
                 <div class="bottom columns">
                   <div class="column"><p class="subtitle">100</p></div>
                   <div class="column"><p class="subtitle">1900</p></div>
@@ -29,7 +33,7 @@
 </template>
 
 <script>
-// import firebase from "firebase/app";
+import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import Hero from "../../components/Hero";
@@ -39,10 +43,56 @@ export default {
   components: { Hero },
 
   data() {
-    return {};
+    return {
+      plusData: [],
+      minusData: [],
+      plusMinus: "",
+      loading: false,
+    };
   },
 
-  methods: {},
+  methods: {
+    get() {
+      // function for adding number in array (where they are given as string)
+      const add = (x) => x.map(Number).reduce((a, v) => a + v);
+      const result = add(this.plusData) - add(this.minusData);
+      // checking if number is positive or negative and returning positive number wih +
+      this.plusMinus = result[0] === "-" ? result + " €" : "+" + result  + " €";
+    },
+  },
+
+  // getting informations from DB and making calculations which are been pushed to local arrays
+  beforeMount() {
+    // geting expenses
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .collection("expenses")
+      .get()
+      .then((snapshot) =>
+        // running through the document
+        snapshot.docs.forEach((item) => {
+          // fetching expenses and pushing it to minusData array
+          this.minusData.push(item.data().expenses);
+        })
+      );
+
+    // geting income
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .collection("income")
+      .get()
+      .then((snapshot) =>
+        // running through the document
+        snapshot.docs.forEach((item) => {
+          // fetching expenses and pushing it to plusData array
+          this.plusData.push(item.data().income);
+        })
+      );
+  },
 };
 </script>
 
