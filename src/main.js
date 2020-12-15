6a8109937494f7884878a198fad050ed3a84a476
Ramunas <zamrif@gmail.com>
2020-12-15 13:11:17 +0200

    firebase link updated

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./servises/firebase.dev";
+import "./servises/firebase";
 import "bulma/css/bulma.min.css";
 
 Vue.config.productionTip = false;
